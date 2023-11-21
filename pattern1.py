21c85240fa710169dc9e15bb6bd0f21a4b7fd3dd
prathibha <prathibha.marolix98@gmail.com>
2023-11-21 14:50:44 -0500

    first commit

---
+++ pattern1.py
@@ -0,0 +1,21 @@
+n=5
+for i in range(n):
+    p=69
+    for j in range(i):
+        print('',end=" ")
+    for j in range(i,n):
+        print(chr(p),end=" ")
+        p-=1
+    print()
+    
+    
+#n=5   
+#for i in range(n):
+ #  for j in range(i):
+  #     print(" ",end=" ")
+  # for j in range(i,n):
+   #    if(i%2==0):
+    #       print('Z',end=" ")
+    #   else:
+     #      print('A',end=" ")
+  # print()
