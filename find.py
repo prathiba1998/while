71eab7aca474b0dbb005e4d8f5e9ab9984ab9edb
prathiba1998 <150787054+prathiba1998@users.noreply.github.com>
2023-11-21 06:04:31 -0500

    Add files via upload

---
+++ find.py
@@ -0,0 +1,7 @@
+a="her name is prathibha and prathibha is from vizianagaram"
+print(a.index("is"))
+print(a.index("ram"))
+print(a.index("vizianagaram"))
+print(a.index("prathibha"))
+print(a.index("he"))
+print(a.index("shyam"))
---
+++ pattern.py
@@ -0,0 +1,38 @@
+#n=5
+#p=0
+#for i in range(n):
+   # for j in range(i+1):
+    #    print(p,end=" ")
+    #p+=3
+    #print()
+    
+#n=6
+#for i in range(n):
+ #   for j in range(i+1):
+  #      if(i%2!=0):
+   #         print(4,end=" ")
+#   else:
+#       print(5,end=" ")
+#print()
+             
+#n=5    
+#for i in range(n):
+ # p=1    
+  #for j in range(i+1):
+   # print(' ',end=" ")
+  #for j in range(i,n):
+   # print(p,end=" ")
+    #p+=1
+  #print()
+  
+  
+  
+n=5
+p=1    
+for i in range(n):
+    for j in range(i+1):
+      print(p,end=" ")
+      p+=1
+    print()  
+  
+  
