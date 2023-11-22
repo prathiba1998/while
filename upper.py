13bf4ffd5adb20fc91469ee3e1508125c69e23e0
prathiba1998 <150787054+prathiba1998@users.noreply.github.com>
2023-11-22 00:44:10 -0500

    Add files via upload

---
+++ upper.py
@@ -0,0 +1,27 @@
+#upper() used to convert lowercase letters in a string to uppercase
+#s="hii i am prathibha"
+#a=s.upper()
+#print(a)
+
+
+
+#lower() used to convert uppercase letters in a string to lowercase
+#s="HELLO GOOD MORNING"
+#b=s.lower()
+#print(b)
+
+#swapcase() used for converting uppercase to lowercase and lowercase to uppercase
+#s="This IS Prathibha"
+#c=s.swapcase()
+#print(c)
+
+
+
+#title()  used to convert the initial letter of each word in a string to a capital letter
+#s="ram is good boy"
+#print(s.title())
+
+
+#capitalize()  used to convert only first letter of string
+s="i am good"
+print(s.capitalize())
