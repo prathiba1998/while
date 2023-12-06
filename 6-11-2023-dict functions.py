52bd9cf074794693e04b2d6ae6d42c1edacf9311
prathiba1998 <150787054+prathiba1998@users.noreply.github.com>
2023-12-06 06:44:02 -0500

    Add files via upload

---
+++ 6-11-2023-dict functions.py
@@ -0,0 +1,60 @@
+#d={100:"ram",200:"prathibha",300:"shyam",400:"sashi",500:"apple",600:"banana"}
+#del d[100]
+#del d[200]
+#del d[300]
+#del d[400]
+#del d[500]
+#print(d)
+
+
+#clear()
+#d={100:"ram",200:"prathibha",300:"shyam",400:"sashi",500:"apple",600:"banana"}
+#d.clear()
+
+#list=["prabha","apple","grapes"]
+#d={1:list}
+#print(d)
+
+
+#d=dict([(100,"ram"),(200,"prathibha"),(300,"shyam"),(400,"sashi"),(500,"apple")])
+#print(d)
+
+
+
+d=dict([(100,"ram"),(200,"prathibha"),(300,"shyam"),(400,"sashi"),(500,"apple")])
+#print(d.get(1000))
+#print(d.get(400))
+#print(d.get(800,"cherry"))
+#print(d.get(100))
+#print(d.get(900))
+#print(d)
+
+
+
+
+#d=dict([(100,"ram"),(200,"prathibha"),(300,"shyam"),(400,"sashi"),(500,"apple")])
+#print(d.pop(400))
+#print(d.pop(200))
+#print(d.pop(100))
+#print(d.pop(300))
+#print(d.pop(500))
+#print(d)
+
+
+d=dict([(100,"ram"),(200,"prathibha"),(300,"shyam"),(400,"sashi"),(500,"apple")])
+#print(d.popitem())
+#print(d.popitem())
+#print(d.popitem())
+#print(d.popitem())
+#print(d)
+
+
+
+d=dict([(100,"ram"),(200,"prathibha"),(300,"shyam"),(400,"sashi"),(500,"apple")])
+print(d.keys())
+print(d.values())
+print(d.items())
+print(d)
+
+
+
